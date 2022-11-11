086ab43bc711af1543976025d7d727a31a9c57b6
Brandonduce <Brandonduce12@gmail.com>
2022-11-10 22:18:15 -0300

    terminado

--- SacarEntradas.js
+++ SacarEntradas.js
@@ -1,5 +1,3 @@
-
-
 let peliculas = [{
     nombre: "DC LIGA DE SUPER MASCOTAS",
     rutaImg: "./img/pel1.jpg",
@@ -9,16 +7,79 @@ let peliculas = [{
     claseImg: "filtro",
     clase2: "cont-img",
     claseTitulo: "Nombre-pelicula"
-},{
-        nombre: "TREN BALA",
-        rutaImg: "./img/pel2.jpg",
-        id: "98",
-        claseDelpadre: "general",
-        claseHijo: "general__imagen",
-        claseImg: "filtro",
-        clase2: "cont-img",
-        claseTitulo: "Nombre-pelicula"
-    }]
+}, {
+    nombre: "TREN BALA",
+    rutaImg: "./img/pel2.jpg",
+    id: "98",
+    claseDelpadre: "general",
+    claseHijo: "general__imagen",
+    claseImg: "filtro",
+    clase2: "cont-img",
+    claseTitulo: "Nombre-pelicula"
+}, {
+    nombre: "THOR AMOR Y TRUENO",
+    rutaImg: "./img/pel3.jpg",
+    id: "97",
+    claseDelpadre: "general",
+    claseHijo: "general__imagen",
+    claseImg: "filtro",
+    clase2: "cont-img",
+    claseTitulo: "Nombre-pelicula"
+}, {
+    nombre: "JURASSIC WORLD: DOMINION",
+    rutaImg: "./img/pel4.jpg",
+    id: "96",
+    claseDelpadre: "general",
+    claseHijo: "general__imagen",
+    claseImg: "filtro",
+    clase2: "cont-img",
+    claseTitulo: "Nombre-pelicula"
+}, {
+    nombre: "LIGHTYEAR",
+    rutaImg: "./img/pel5.jpg",
+    id: "95",
+    claseDelpadre: "general",
+    claseHijo: "general__imagen",
+    claseImg: "filtro",
+    clase2: "cont-img",
+    claseTitulo: "Nombre-pelicula"
+}, {
+    nombre: "MINIONS: NACE UN VILLANO",
+    rutaImg: "./img/pel6.jpg",
+    id: "94",
+    claseDelpadre: "general",
+    claseHijo: "general__imagen",
+    claseImg: "filtro",
+    clase2: "cont-img",
+    claseTitulo: "Nombre-pelicula"
+}, {
+    nombre: "TOP GUN: MAVERICK",
+    rutaImg: "./img/pel7.jpg",
+    id: "93",
+    claseDelpadre: "general",
+    claseHijo: "general__imagen",
+    claseImg: "filtro",
+    clase2: "cont-img",
+    claseTitulo: "Nombre-pelicula"
+}, {
+    nombre: "LA HABITACIÓN DEL HORROR",
+    rutaImg: "./img/pel8.jpg",
+    id: "92",
+    claseDelpadre: "general",
+    claseHijo: "general__imagen",
+    claseImg: "filtro",
+    clase2: "cont-img",
+    claseTitulo: "Nombre-pelicula"
+}, {
+    nombre: "LA GALLINA TURULECA",
+    rutaImg: "./img/pel9.jpg",
+    id: "91",
+    claseDelpadre: "general",
+    claseHijo: "general__imagen",
+    claseImg: "filtro",
+    clase2: "cont-img",
+    claseTitulo: "Nombre-pelicula"
+}]
 
 let contenedor = document.getElementById("contenedorPadre")
 
@@ -31,8 +92,6 @@ peliculas.forEach((peliculas) => {
     <div class="cont-img">
         <h3 class="Nombre-pelicula">${peliculas.nombre}</h3>                      
     </div>`
-
-
 
     contenedor.appendChild(divPadre)
 }) 
